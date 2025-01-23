eb5bd723d8a6f4205096d130c9cc7db2967301fa
MD TARIQUL ISLAM <tariqul.developer@gmail.com>
2025-01-23 09:19:13 +0600

    footer done

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Shared/Navbar";
+import Footer from "@/components/Shared/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,9 +27,10 @@ export default function RootLayout({ children }) {
         <header>
           <Navbar />
         </header>
-        <main className="w-10/12 mx-auto">
+        <main className="w-10/12 mx-auto min-h-[calc(100vh-144px)]">
           {children}
         </main>
+        <Footer />
       </body>
     </html>
   );
