cbabeabc7bd38892eb0d5a1868349c9ccee7f067
MD TARIQUL ISLAM <tariqul.developer@gmail.com>
2025-01-23 18:31:57 +0600

    add midleware

---
+++ src/app/profile/loading.jsx
@@ -0,0 +1,9 @@
+const loading = () => {
+    return (
+        <div className="text-center flex justify-center items-center h-screen">
+            Profile is Loading...
+        </div>
+    );
+};
+
+export default loading;
